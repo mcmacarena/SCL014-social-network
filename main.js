c3373f445b85503568a605d076744d1d3d6fb8f8
Macarena Cuevas Guzmán <macarenacuevasg@gmail.com>
2020-08-06 17:45:09 -0400

    06-08-2020 login casi casi listo

--- main.js
+++ main.js
@@ -1,44 +1,24 @@
 // Este es el punto de entrada de tu aplicacion
 import { contentInitialPage, contentSignIn, contentHome } from './lib/index.js';
-import { registerNewUser, logInUser } from './lib/firebase.js'
 
 const render = (hash) => {
   const screen = document.getElementById('screens');
-  if(hash === ''){
+  if (hash === '') {
     contentInitialPage();
   }
   if (hash === '#/SignIn') {
-    screen.innerHTML='';
+    screen.innerHTML = '';
     contentSignIn()
   }
-  else if (hash === '#/createProfile') {
-    screen.innerHTML='';
+  else if (hash === '#/Home') {
+    screen.innerHTML = '';
     contentHome()
-  } 
+  }
 }
 const changePage = () => {
   render(window.location.hash);
 }
+
 window.addEventListener('hashchange', changePage);
 window.addEventListener('load', changePage)
-
-
-
-  
-
-  
-
-
-// const logIn = () => {
-//   const email = document.querySelector('#newUserEmail').value;
-//   const password = document.querySelector('#newUserPassword').value;
-
-//   document.querySelector('#btnLogin').addEventListener('click', logInUser(email, password));
-// }
-
-
-
-
-
-
 
